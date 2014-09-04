34dda27791000e0ede5768f2ffbe2d55c793ca39
Pavan Tej Medapuram <pavan.teju@yahoo.com>
2014-09-03 19:25:46 -0700

    completed read/save items

--- src/com/pavantej/simpletodo/TodoActivity.java
+++ src/com/pavantej/simpletodo/TodoActivity.java
@@ -1,15 +1,38 @@
 package com.pavantej.simpletodo;
 
-import android.os.Bundle;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.commons.io.FileUtils;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class TodoActivity extends Activity {
 
+	ListView lvItems;
+	ArrayList<String> items;
+	ArrayAdapter<String> itemsAdapter;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo);
+        lvItems = (ListView) findViewById(R.id.lvTodoItems);
+        items = new ArrayList<String>();
+        readItems();
+        itemsAdapter = new ArrayAdapter<String>(this,
+        		android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        setupListViewListener();
     }
 
 
@@ -19,5 +42,46 @@ public class TodoActivity extends Activity {
         getMenuInflater().inflate(R.menu.todo, menu);
         return true;
     }
+    
+    public void addTodoItem(View v){
+    	EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+    	itemsAdapter.add(etNewItem.getText().toString());
+    	etNewItem.setText("");
+    	saveItems();
+    }
 
+    private void setupListViewListener(){
+    	lvItems.setOnItemLongClickListener(new OnItemLongClickListener(){
+    		@Override
+    		public boolean onItemLongClick(AdapterView<?> parent,
+    				View v, int position, long rowId){
+    			items.remove(position);
+    			itemsAdapter.notifyDataSetChanged();
+    			saveItems();
+    			return true;
+    		}
+    	});
+    }
+    
+    private void readItems(){
+    	File itemsFile = getFilesDir();
+    	File todoFile = new File(itemsFile, "todo.txt");
+    	try {
+			items = new ArrayList<String>(FileUtils.readLines(todoFile));
+		} catch (IOException e) {
+			items = new ArrayList<String>();
+			e.printStackTrace();
+		}
+    }
+    
+    private void saveItems(){
+    	File itemsFile = getFilesDir();
+    	File todoFile = new File(itemsFile, "todo.txt");
+    	try {
+			FileUtils.writeLines(todoFile, items);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
 }
